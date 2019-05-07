4b495038570c0561e4d8ecd861d7c8639458ca28
Jenn J Will <jennijwill@gmail.com>
2019-05-07 15:48:27 -0700

    minor changes made before branching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDOjsirnJ4vFVnnA_AzADnv9sVUnY9amZs",
+  authDomain: "bloc-chat-3651a.firebaseapp.com",
+  databaseURL: "https://bloc-chat-3651a.firebaseio.com",
+  projectId: "bloc-chat-3651a",
+  storageBucket: "bloc-chat-3651a.appspot.com",
+  messagingSenderId: "783885462025",
+  appId: "1:783885462025:web:e0c89896ae16ca1a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
